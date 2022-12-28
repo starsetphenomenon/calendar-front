419e18e82215a498c2f13d48607723589c34f448
Пользователь <starsetphenomenon@gmail.com>
2022-12-28 14:31:10 +0200

    Auth added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
         private store: Store<{ appState: AppState }>
     ) { }
 
-    BASE_URL: string = 'http://localhost:3333';
+    BASE_URL: string = 'https://calendar-back-production-dfa3.up.railway.app';
     API: string = 'api/users';
     user!: User;
 
